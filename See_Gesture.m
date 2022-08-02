11cbf1bc018ab04a65856381674f670a48cd6b82
张欣 <shenshikexmu@163.com>
2022-08-02 16:10:28 +0800

    Upadata  quaternProd
    Upadata  quaternProd

--- See_Gesture.m
+++ See_Gesture.m
@@ -203,6 +203,14 @@ function q = axisAngle2quatern(axis, angle)
 end
 
 function ab = quaternProd(a, b)
+
+    %  b=quaternProd([-a(1),a(2:4)], ab)
+
+    % ab(1)  =[b(1), -b(2), -b(3), -b(4);...   a(1)
+    % ab(2)    b(2),  b(1),  b(4), -b(3);...   a(2)
+    % ab(3)    b(3), -b(4),  b(1),  b(2);...   a(3)
+    % ab(4)    b(4),  b(3), -b(2),  b(1)]      a(4)
+    
     ab(1) = a(1)*b(1)-a(2)*b(2)-a(3)*b(3)-a(4)*b(4);
     ab(2) = a(1)*b(2)+a(2)*b(1)+a(3)*b(4)-a(4)*b(3);
     ab(3) = a(1)*b(3)-a(2)*b(4)+a(3)*b(1)+a(4)*b(2);
